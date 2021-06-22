1c700200d54ba6eba3484034469b484626579733
Mikmondi <mikmondi@gmail.com>
2021-06-23 01:18:19 +0300

    added atets class for sighting

---
+++ src/test/java/models/DatabaseRule.java
@@ -0,0 +1,28 @@
+package models;
+
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+        //development database
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "moringa", "gre8tness");  //Those with linux or windows use two strings for username and password
+    }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            String resetAnimalsId = "ALTER SEQUENCE animals_id_seq RESTART WITH 1;;";
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
+            String resetSightingsId = "ALTER SEQUENCE sightings_sighting_id_seq RESTART WITH 1;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(resetAnimalsId).executeUpdate();
+            con.createQuery(deleteSightingsQuery).executeUpdate();
+            con.createQuery(resetSightingsId).executeUpdate();
+        }
+    }
+
+}
---
+++ src/test/java/models/SightingTest.java
@@ -0,0 +1,68 @@
+package models;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SightingTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+    public Sighting setupObject() {
+        return new Sighting(1, "Pembeni", "Douglas Onzima");
+    }
+
+    @Test
+    public void sighting_instantiatesCorrectly_true() {
+        assertEquals(true, setupObject() instanceof Sighting);
+    }
+    @Test
+    public void getAnimalId_sightingInstantiatesWithAnimalId_int() {
+        assertEquals(1, setupObject().getAnimalId());
+    }
+    @Test
+    public void getLocation_sightingInstantiatesWithLocation_string() {
+        assertEquals("Pembeni", setupObject().getLocation());
+    }
+    @Test
+    public void getRangerName_sightingInstantiatesWithRangerName_string() {
+        assertEquals("Douglas Onzima", setupObject().getRangerName());
+    }
+
+    @Test
+    public void equals_returnsTrueIfLocationAndAnimalIdAreSame_true() {
+        Sighting firstSighting = new Sighting(1, "pembeni", "Ben");
+        Sighting secondSighting = new Sighting(1, "pembeni", "Ben");
+        assertTrue(firstSighting.equals(secondSighting));
+    }
+    @Test
+    public void save_insertsObjectIntoDatabase_Sighting() {
+        setupObject().save();
+        assertTrue(Sighting.all().get(0).equals(setupObject()));
+    }
+    @Test
+    public void save_assignsIdToObject() {
+        Sighting testSighting = new Sighting(1, "pembeni", "Ben");
+        testSighting.save();
+        Sighting savedSighting = Sighting.all().get(0);
+        assertEquals(testSighting.getSightingId(), savedSighting.getSightingId());
+    }
+    @Test
+    public void all_returnsAllInstancesOfSighting_true() {
+        Sighting firstSighting = new Sighting(1, "pembeni", "Ben");
+        firstSighting.save();
+        Sighting secondSighting = new Sighting(56, "Daraja", "Ruwel");
+        secondSighting.save();
+        assertEquals(true, Sighting.all().get(0).equals(firstSighting));
+        assertEquals(true, Sighting.all().get(1).equals(secondSighting));
+    }
+    @Test
+    public void find_returnsSightingWithSameId_secondSighting() {
+        Sighting firstSighting = new Sighting(1, "pembeni", "Ben");
+        firstSighting.save();
+        Sighting secondSighting = new Sighting(2, "Daraja", "Ruwel");
+        secondSighting.save();
+        assertEquals(Sighting.find(secondSighting.getSightingId()), secondSighting);
+    }
+}
